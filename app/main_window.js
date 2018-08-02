aac120f342cac345021c8cbe9870cd0aaf54d9f9
Stéphane G <stephane.gronowski@gmail.com>
2018-08-02 07:42:36 +0200

    First commit : basic app, not tray

---
+++ app/main_window.js
@@ -0,0 +1,41 @@
+const electron = require('electron');
+const {
+    BrowserWindow
+} = electron;
+
+class MainWindow extends BrowserWindow {
+
+    constructor(loadURL, iconPath, optionnals) {
+        super({
+            height: 500,
+            width: 300,
+            // No window decoration
+            frame: true,
+            // Block window rezise
+            resizable: true,
+            show: true,
+            // Needed on linux to show icon on taskbar (if wants to show in the task bar)
+            icon: iconPath,
+            // Won't show the tray icon app on taskBar, app.dock.hide() for mac
+            skipTaskbar: false,
+            webPreferences: {
+                // Avoid freeze of the app when no focused (tray)
+                backgroundThrottling: false
+            }
+        });
+
+        this.loadURL(loadURL);
+
+        if (optionnals) {
+            if (optionnals.hideOnBlur) {
+                this.on('blur', this.onBlur.bind(this));
+            }
+        }
+    }
+
+    hideWindow() {
+        this.hide();
+    }
+}
+
+module.exports = MainWindow;
---
+++ index.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const electron = require('electron');
+const AppTray = require('./app/app_tray');
+const MainWindow = require('./app/main_window');
+
+const {
+    app,
+    ipcMain
+} = electron;
+
+// if need to hide the app from taskbar/dock
+const hideApp = false;
+// Set up a tray icon
+const useTray = false;
+
+// Init on ready
+let isLinux;
+let isWindows;
+let isMac;
+
+// Keeps reference to avoid garbage collection 
+let mainWindow;
+let tray;
+
+
+app.on('ready', () => {
+    console.log('App is ready');
+    console.log(`Platform is ${process.platform}`);
+
+    // Update plateform info
+    isMac = process.platform === 'darwin';
+    isWindows = process.platform === 'win32';
+    isLinux = !isWindows && !isMac;
+
+    const appIcon = !isMac ? 'windows-icon.png' : 'iconTemplate.png';
+    const appIconPath = path.join(__dirname, `./src/assets/${appIcon}`);
+
+    // (macOs only) Hide the tray app on the dock 
+    if (hideApp && isMac) {
+        app.dock.hide();
+    }
+
+    const loadURL = `file://${__dirname}/src/index.html`;
+    mainWindow = new MainWindow(loadURL, appIconPath);
+
+    if (useTray) {
+        let optionals = {
+            hideOnBlur : false
+        }
+        tray = new AppTray(appIconPath, mainWindow, optionals);
+    }
+});
